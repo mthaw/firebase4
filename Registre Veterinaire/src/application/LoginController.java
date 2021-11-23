6ee8a46f39ea221f443be3df34fe408cd223fcde
mthaw <mthaw@10.6.1.98>
2021-11-23 10:49:34 -0500

    Ajout de Login et Register (register pas encore fini: besoin de finir le fichier FXML et puis le controller.

---
+++ Registre Veterinaire/src/application/LoginController.java
@@ -0,0 +1,13 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField txtuser;
+
+    @FXML
+    private PasswordField txtpasse;
+
+}
