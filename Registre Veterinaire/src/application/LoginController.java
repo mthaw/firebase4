8df83551a159464ecd591dd22e84c760cccffb5d
mthaw <mthaw@10.6.1.75>
2021-12-01 14:13:48 -0500

    Push fin de cours

--- Registre Veterinaire/src/application/LoginController.java
+++ Registre Veterinaire/src/application/LoginController.java
@@ -1,16 +1,36 @@
 package application;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
-public class LoginController {
+public class LoginController implements Initializable {
 
     @FXML
     private TextField txtuser;
 
     @FXML
     private PasswordField txtpasse;
+    
+    List<Animal> animauxList;
+    
+    public static Animal animal;
+    
+    public static Animal currentAnimal;
+    
+    int animalFound = 0;
 
     
     public void Register() {
@@ -22,4 +42,33 @@ public class LoginController {
     	new Main().openMainWindow();
     	Main.loginStage.close();
     }
+    
+    public void Connect(ActionEvent event) {
+    	Firebase firebase = new Firebase("https://martinfiletest-default-rtdb.firebaseio.com/");
+    	firebase.child("animaux").addListenerForSingleValueEvent(new ValueEventListener() {
+			
+			@Override
+			public void onDataChange(DataSnapshot dataSnapshot) {
+				for(DataSnapshot data: dataSnapshot.getChildren()) {
+					Animal animal = data.getValue(Animal.class);
+					if(animal.getUser().equals(txtuser.getText()) && animal.getPasse().equals(txtpasse.getText())) {
+						animal.setId(data.getKey());
+						animalFound=1;
+						currentAnimal=animal;
+						animauxList.add(animal);
+					}
+				}
+			}
+			
+			@Override
+			public void onCancelled(FirebaseError arg0) {
+				
+			}
+		});
+    }
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		animauxList = new ArrayList();
+	}
 }
--- Registre Veterinaire/src/application/RegisterController.java
+++ Registre Veterinaire/src/application/RegisterController.java
@@ -14,6 +14,8 @@ import javafx.scene.control.TextField;
 
 public class RegisterController implements Initializable{
 
+	//Definition de tous les champs pour l'enregistrement
+	
     @FXML
     private TextField tphone;
 
@@ -40,6 +42,9 @@ public class RegisterController implements Initializable{
     
     private ObservableList<String> list = FXCollections.observableArrayList("Gentil", "Gourmand", "Paresseux", "Léthargique", "Aggressif");
 
+    /**
+     * Methode qu'on appelle en ouvrant le fenetre d'enregistrement. Il nous prepare le combobox en en attribuant les options (items).
+     */
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		cbocpt.setItems(list);
@@ -49,6 +54,15 @@ public class RegisterController implements Initializable{
 		Firebase firebase = new Firebase("https://martinfiletest-default-rtdb.firebaseio.com/");
 		Animal animal = new Animal();
 		animal.setNom(tnom.getText());
+		animal.setAddresse(taddresse.getText());
+		animal.setPasse(tpasse.getText());
+		animal.setPhone(tphone.getText());
+		animal.setComportement(cbocpt.getValue());
+		animal.setAge(tage.getText());
+		animal.setUser(tusager.getText());
+		firebase.child("animaux").push().setValue(animal);
+		new Main().openLoginWindow();
+		Main.registerStage.close();
 	}
 
 
